36e46209969674210a52971f37a4334a989d62a0
shanelgarcia <60453441+shanelgarcia@users.noreply.github.com>
2021-01-16 00:15:08 -0700

    Filter search album controller get album for artistid

--- ChinookSln/ChinookSystem/BLL/AlbumController.cs
+++ ChinookSln/ChinookSystem/BLL/AlbumController.cs
@@ -30,5 +30,23 @@ namespace ChinookSystem.BLL
                 return results.ToList();
             }
         }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ArtistAlbums> Albums_GetAlbumsForArtist(int artistid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    where x.ArtistID == artistid
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name,
+                                                        ArtistId = x.ArtistID                                                       
+                                                    };
+                return results.ToList();
+            }
+        }
     }
 }
